d7d4a8b012868212dd8ad48814fb907f3fe00e89
iliko13 <107374131+iliko13@users.noreply.github.com>
2024-11-30 00:04:00 +0400

    creating Login and SignUp, tabbar

---
+++ Stay_Connected/Application/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  Stay_Connected
+//
+//  Created by iliko on 11/29/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        let isLoggedIn = false
+        
+        if isLoggedIn {
+            window.rootViewController = UINavigationController(rootViewController: TabBarController())
+        } else {
+            window.rootViewController = UINavigationController(rootViewController: LoginVC())
+        }
+
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Stay_Connected/Screens/Profile/ProfileVC.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Stay_Connected
+//
+//  Created by iliko on 11/29/24.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+
+    }
+
+}
