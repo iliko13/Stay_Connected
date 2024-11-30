d1873d7a8f5d7b26caaa92f1d3e8330e57f7b684
sandromaraneli <154780179+sandromaraneli@users.noreply.github.com>
2024-11-30 14:50:40 +0400

    Configured Profile page UI

---
+++ Stay_Connected/Stay_Connected/Screens/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Stay_Connected
+//
+//  Created by iliko on 11/29/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+
+    }
+
+
+}
